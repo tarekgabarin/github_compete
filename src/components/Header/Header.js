1edd4df29782a2f4da3ce10387e342edeeabfbec
tarekgabarin <tarekgabarin1992@gmail.com>
2019-08-01 13:47:37 -0400

    added tachyon to project and started work on navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import logo from '../../logo.jpg'
 
-export default () => (
-    <header className='tc pv4 pv5-ns'>
-        <img src={logo} className='br-100 pa1 ba b--black-10 h3 w3' alt='avatar' />
-        <h1 className='f5 f4-ns fw6 mid-gray'>Jasper Whitehouse</h1>
-        <h2 className='f6 gray fw2 ttu tracked'>Los Angeles</h2>
-    </header>
-)
+export default function Header(props) {
+    return (
+        <nav className={'fl w-100 bg-navy near-white h-25 pa3 bb'}>
+            <span>Header</span>
+        </nav>
+    )
+}
