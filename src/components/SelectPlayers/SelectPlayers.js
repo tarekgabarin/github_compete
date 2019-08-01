50fc93465c3e2bd4f710d2780b4fefc582e1a16b
tarekgabarin <tarekgabarin1992@gmail.com>
2019-08-01 17:00:54 -0400

    added placeholder text for input text

--- src/components/SelectPlayers/SelectPlayers.js
+++ src/components/SelectPlayers/SelectPlayers.js
@@ -9,7 +9,7 @@ function SelectPlayers(props) {
 
                 <div className={'mb1'}>
                     <h1 className={'mb0'}>Player One</h1>
-                    <input type="text" className={'input-reset pa1 w-100 h2 ba b--black br2'}/>
+                    <input type="text" placeholder={'github username'} className={'input-reset pa1 w-100 h2 ba b--black br2'}/>
                 </div>
 
                 <div className="tc dark-red">
@@ -18,7 +18,7 @@ function SelectPlayers(props) {
 
                 <div className={'mb3'}>
                     <h1 className={'mb0 mt0 tr'}>Player Two</h1>
-                    <input type="text" className={'input-reset pa1 w-100 h2 ba b--black br2'}/>
+                    <input type="text" placeholder={'github username'} className={'input-reset pa1 w-100 h2 ba b--black br2'}/>
                 </div>
 
                 <div>
